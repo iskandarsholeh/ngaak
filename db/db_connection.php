e971113eefc46b172a0106a120e9751f978cb20e
Iskandar Sholeh <79823292+iskandarsholeh@users.noreply.github.com>
2021-07-03 21:14:42 +0700

    update finishing

--- db/db_connection.php
+++ db/db_connection.php
@@ -2,7 +2,7 @@
 session_start();
 session_regenerate_id(true);
 // change the information according to your database
-$db_connection = mysqli_connect("localhost","root","","coding");
+$db_connection = mysqli_connect("us-cdbr-east-04.cleardb.com","b3a1bf7a01f094","248d647e","heroku_dc3a34e1ef0430a");
 // CHECK DATABASE CONNECTION
 if(mysqli_connect_errno()){
     echo "Connection Failed".mysqli_connect_error();
